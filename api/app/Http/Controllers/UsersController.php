29bbe83f6f12c9eb1b6d9535173399d61ff84f83
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2022-07-06 18:02:49 -0300

    users

--- api/app/Http/Controllers/UsersController.php
+++ api/app/Http/Controllers/UsersController.php
@@ -4,28 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Validation\ValidationException;
 
 class UsersController extends Controller
 {
 
 
-    public function login(Request $input){
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            //'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
 
 
-        $credentials = $input->only("email","password");
-
-        /* if(!auth()->attempt($credentials)){
-            abort(401,"Invalid credentials");
+         if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
-        $token = auth()->user()->getRememberToken(); */
-
-        $array = [
-            "token"=>"ASDLKF;JA;SDLFAUISDFAJSD;FJAKSD;F"
-        ];
-
-        return JsonResponseController::get($array);
     }
 
 
@@ -35,13 +37,13 @@ class UsersController extends Controller
         $validator = Validator::make($request->post(), [
             'email' => 'required|email|unique:users,email|max:255',
             'name' => 'required|string',
+            'password'=>'required',
             "type_user" => "required|numeric"
         ]);
 
 
         if ($validator->fails()) {
            return JsonResponseController::error($validator->errors()->first());
-
         }
 
         $user = new User;
